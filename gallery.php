c71772169286d7c4f02101ce1451e1035225054b
win0908 <106888234+win0908@users.noreply.github.com>
2023-10-08 12:11:45 -0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,6 +1,9 @@
 <?php
-
     echo "<h1>Gallery!</h1>";
-    echo '<a href="http://www.win0908.me/it261/index.php"> Home </a><br>';
-    $my_github = "https://github.com/win0908/it261/tree/main";
-    echo "<h3> Check out my code in <a href='".$my_github."'> GitHub </a> </h3>";
+
+    echo '<table style="width:25%">';
+    echo '  <tr>';
+    echo '      <td><a href="http://www.win0908.me/it261/index.php"> Home </a>';
+    echo '      <td><a href="https://github.com/win0908/it261/tree/main"> GitHub </a>';
+    echo '  </tr>';
+    echo '</table>';
