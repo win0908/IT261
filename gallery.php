49c9e5c72689697f6ee6e3aed10e5866711c7e3c
win0908 <106888234+win0908@users.noreply.github.com>
2023-10-08 12:50:43 -0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -3,7 +3,7 @@
 
     echo '<table style="width:25%">';
     echo '  <tr>';
-    echo '      <td><a href="http://www.win0908.me/it261/index.php"> Home </a>';
+    echo '      <td><a href="http://www.win0908.me/it261/website/index.php"> Home </a>';
     echo '      <td><a href="https://github.com/win0908/it261/tree/main"> GitHub </a>';
     echo '  </tr>';
     echo '</table>';
