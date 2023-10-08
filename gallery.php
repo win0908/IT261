e65bc17f5a5016e394e56710a36194fe8691e210
win0908 <106888234+win0908@users.noreply.github.com>
2023-10-08 12:07:36 -0700

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,6 @@
+<?php
+
+    echo "<h1>Gallery!</h1>";
+    echo '<a href="http://www.win0908.me/it261/index.php"> Home </a><br>';
+    $my_github = "https://github.com/win0908/it261/tree/main";
+    echo "<h3> Check out my code in <a href='".$my_github."'> GitHub </a> </h3>";
