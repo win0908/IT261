116efbe64313eb31358bfc08a19bb697a5a65e73
win0908 <106888234+win0908@users.noreply.github.com>
2023-10-20 20:45:52 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+
+        <div id="header">
+            <a href="http://www.win0908.me/it261">  <h1 id="header_text"> Win0908 </h1>   </a> 
+            <div id="container_header_link">
+                <div id="container_header_link1">
+                    <a href="http://www.win0908.me/it261/website/about.php">   About   </a>
+                    <a href="http://www.win0908.me/it261/website/contact.php"> Contact </a>
+                    <a href="http://www.win0908.me/it261/website/daily.php">   Daily   </a>
+                </div>
+                <div id="container_header_link2"> <!-- id="container_head_link" -->
+                    <a href="http://www.win0908.me/it261/website/gallery.php"> Gallery </a>
+                    <a href="http://www.win0908.me/it261/website/project.php"> Project </a>
+                    <a href="https://github.com/win0908/it261/tree/main"> GitHub </a>
+                </div>
+            </div>
+        </div>
