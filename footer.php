5f69e0debc84ca5181014b69347cd8e7b5bd0728
win0908 <106888234+win0908@users.noreply.github.com>
2023-10-20 20:46:19 -0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+    <span id="footer_name"> Web Design By Supawin Soongsawang </span>
+    <span> 
+        Copyright &copy;2022  All Rights Reserved,<a href="https://validator.w3.org/nu/?doc=http://www.win0908.me/it261/index.php"> HTML Validation</a> <a href="https://jigsaw.w3.org/css-validator/validator?uri=http://www.win0908.me/it261/index.php">CSS Validation </a>
+    </span>
+</div>
