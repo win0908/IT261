07fed5a0df25683a7b425b1a9b5b737cdf3141da
win0908 <106888234+win0908@users.noreply.github.com>
2023-10-21 04:53:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 
 
+
 <!DOCTYPE html>
 
 <html>
 <head>
     <style>
-    
-    #bgimg {
-        background-image: url("https://images.unsplash.com/photo-1697371136373-d4e896c530f1?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w5MTMyMXwwfDF8YWxsfDE0fHx8fHx8Mnx8MTY5Nzc1NDY2NXw&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=200");
-         background-size: 300px 100px;
-    }
     #container {
         width: 100%;
         display: flex;
@@ -115,80 +111,13 @@
         border-radius: 10px;
         border-color: #F1F1F1;
     }
-    
-        a { 
-        text-decoration: none;
-        color: #57A1FF;
-        font-size: 24px;
-    }
-    #container_header {
-        width: 100%;
-        display: flex;
-        overflow: auto;
-        align-items: flex-start;
-        flex-direction: column;
-        justify-content: flex-start;
-    }
-    #header {
-        flex: 0 0 auto;
-        width: 90%;
-        height: 144px;
-        margin: 00px;
-        display: flex;
-        align-self: center;
-        margin-top: 0px;
-        align-items: center;
-        margin-left: 0px;
-        margin-right: 0px;
-        margin-bottom: 0px;
-        justify-content: space-between;
-    }
-    #header_text {
-        font-size: 84px;
-    }
-    #container_header_link {
-        flex: 0 0 auto;
-        width: auto;
-        height: auto;
-        display: flex;
-        align-items: flex-start;
-    }
-    #container_header_link1 {
-        flex: 0 0 auto;
-        width: 96px;
-        height: auto;
-        display: flex;
-        align-items: flex-end;
-        flex-direction: column;
-    }
-    #container_header_link2 {
-        flex: 0 0 auto;
-        width: 96px;
-        height: auto;
-        display: flex;
-        align-items: flex-end;
-        flex-direction: column;
-    }        #footer {
-        flex: 0 0 auto;
-        width: 100%;
-        height: 96px;
-        display: flex;
-        position: relative;
-        align-items: center;
-        margin-top: 24px;
-        margin-bottom: 32px;
-        flex-direction: column;
-        justify-content: flex-start;
-
-        font-size: 16px;
-        white-space: pre;
-        font-family: monospace;
-    }
-    #footer_name {
-        align-self: center;
-    }
-
+    <?php include '/home/dh_yehew8/win0908.me/it261/css/header_css.css';?>
+    <?php include '/home/dh_yehew8/win0908.me/it261/css/footer_css.css';?> 
     </style>
+    
+    
+    
+    
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.7.0/p5.js"></script>
@@ -200,34 +129,18 @@
 
 <body>
 <div id="container">
-    
 
-        <div id="header">
-            <a href="http://www.win0908.me/it261">  <h1 id="header_text"> Win0908 </h1>   </a> 
-            <div id="container_header_link">
-                <div id="container_header_link1">
-                    <a href="http://www.win0908.me/it261/website/about.php">   About   </a>
-                    <a href="http://www.win0908.me/it261/website/contact.php"> Contact </a>
-                    <a href="http://www.win0908.me/it261/website/daily.php">   Daily   </a>
-                </div>
-                <div id="container_header_link2"> <!-- id="container_head_link" -->
-                    <a href="http://www.win0908.me/it261/website/gallery.php"> Gallery </a>
-                    <a href="http://www.win0908.me/it261/website/project.php"> Project </a>
-                    <a href="https://github.com/win0908/it261/tree/main"> GitHub </a>
-                </div>
-            </div>
-        </div>
+    <!-- Header -->
+    <?php include '/home/dh_yehew8/win0908.me/it261/header.php';?>
    
-    
-
     <div id="bodycontainer">
+        
         <!--
         <img src="https://images.unsplash.com/photo-1696801946020-ebc638477c5d?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w5MTMyMXwwfDF8YWxsfDQ0fHx8fHx8Mnx8MTY5NzQzMjAxNXw&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=800" alt="image" id="weeks_img" />
         -->
         <div id="p5">
         <script src="mySketch.js"></script>
         </div>
-        
         
         <div id="bodytextcontainer">
             
@@ -306,14 +219,11 @@
 
 <footer>
     
+<!-- footer -->
+<?php include '/home/dh_yehew8/win0908.me/it261/footer.php';?>
 
-<div id="footer">
-    <span id="footer_name"> Web Design By Supawin Soongsawang </span>
-    <span> 
-        Copyright &copy;2022  All Rights Reserved, <a href="https://validator.w3.org/nu/?doc=http://www.win0908.me/it261/index.php">HTML Validation</a> <a href="https://jigsaw.w3.org/css-validator/validator?uri=http://www.win0908.me/it261/index.php"> CSS Validation</a>
-    </span>
-</div>
 </footer>
 </body>
 </html>
+
 
