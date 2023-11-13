265c7a1d03ff5c3ddcf0f14cb2cbfebf58c72055
win0908 <106888234+win0908@users.noreply.github.com>
2023-11-13 14:23:15 -0800

    Update pictures.php

--- weeks/week7/pictures.php
+++ weeks/week7/pictures.php
@@ -39,9 +39,10 @@ $people['Julian_Castro']    = 'castr_Former Housing/Urban from TX.';
 <table>
 <?php foreach($people as $name => $image)       :?>
 <tr>
-    <td><img src="/it261/images/<?php echo substr( $image, 0, 5);?>.jpg" alt="<?php echo str_replace( '_', ' ', $name);?>"></td>
-    <td><?php echo str_replace( '_', ' ', $name)                                                                      ;?></td>
-    <td><?php echo substr( $image, 6)                                                               ;?></td>
+    <td><img src="/it261/images/<?php echo substr( $image, 0, 5);?>1.jpg" alt="<?php echo str_replace( '_', ' ', $name);?>"></td>
+    <td><?php echo str_replace( '_', ' ', $name)                                                                       ;?></td>
+    <td><?php echo substr( $image, 6)                                                                                  ;?></td>
+    <td><img src="/it261/images/<?php echo substr( $image, 0, 5);?>2.jpg" alt="<?php echo str_replace( '_', ' ', $name);?>"></td>
 </tr>
 <?php endforeach       ;?>
 </table>
