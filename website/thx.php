4116e4d6cf2f7a4ec4dd8c784cf427b178d401dd
win0908 <106888234+win0908@users.noreply.github.com>
2023-11-13 12:17:43 -0800

    Create thx.php

---
+++ website/thx.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container {
+        width: 100%;
+        display: flex;
+        min-height: 100vh;
+        align-items: center;
+        justify-content: center;
+        }
+        .hero-container {
+        width: 100%;
+        display: flex;
+        align-self: center;
+        align-items: center;
+        flex-direction: column;
+        }
+        .hero {
+        color: #01ec04;
+        font-size: 20vh;
+        }
+
+        h1, h3{
+        margin-bottom:0px;
+        margin-top:0px;
+        }
+        
+    </style>
+</head>
+<body>
+
+        <div class="container">
+            <div class="hero-container">
+                <h1 class="hero"> ✔ </h1>
+                <h1> Thank you! </h1>
+                <h3> The form was submitted successfully </h3>
+            </div>
+        </div>
+
+</body>
+</html>
