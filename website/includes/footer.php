50451e7bfccac35529e0a76ba2563f0d3e54e8b3
win0908 <106888234+win0908@users.noreply.github.com>
2023-11-27 20:45:26 -0800

    Add files via upload

---
+++ website/includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <span class="copyright">Copyright ©2022  All Rights Reserved</span>
+    <div class="html-css">
+        <a href="https://validator.w3.org/nu/?doc=http://www.win0908.me/it261/index.php"> HTML Validation</a>
+        <a href="https://jigsaw.w3.org/css-validator/validator?uri=http://www.win0908.me/it261/index.php">CSS Validation </a>
+    </div>
+    <span class="design">Design By Supawin Soongsawang</span>
+</footer>
